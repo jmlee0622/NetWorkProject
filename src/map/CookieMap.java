b7ed88511c6371320e66a949a291242c3b25ae43
jmlee0622 <140807825+jmlee0622@users.noreply.github.com>
2024-11-18 18:52:42 +0900

    1

--- src/map/CookieMap.java
+++ src/map/CookieMap.java
@@ -707,15 +707,15 @@ public class CookieMap extends JFrame {
 
                   if (secondline.equals("MOVE")) {
                      if (thirdline.equals("L")) {
-                        enemyX -= 10;
-                        woonieCurrent("images/woonie_left.png");
+                        enemyX += 10;
+                        woonieCurrent("images/woonie_right.png");
                         woonie.setLocation(enemyX, enemyY);
                         if (enemyX > 580) {
                            enemyX = 580;
                         }
                      } else if (thirdline.equals("R")) {
-                        enemyX += 10;
-                        woonieCurrent("images/woonie_right.png");
+                        enemyX -= 10;
+                        woonieCurrent("images/woonie_left.png");
                         woonie.setLocation(enemyX, enemyY);
                         if (enemyX < 16) {
                            enemyX = 16;
